378654b7301df7c89239284aa360655f8bdcbf22
jharte5 <jonathan.harte@codeimmersives.com>
2020-04-23 12:13:17 -0400

    showing the 1st mini subject in auto component status check good

--- src/components/AutoItems.js
+++ src/components/AutoItems.js
@@ -1,11 +1,19 @@
 import React from 'react'
 import Button from './Button'
 
-export default function AutoItems({onDelete, onUpdate, auto:{service, _id:id}}) {
+export default function AutoItems({onDelete, onUpdate, auto:{service, name, city, price, address, phone, website, _id:id}}) {
     return (
         <div className="ui card" style={{width:"75%", padding:"20px"}}>
             <div className="content">
                 <div className="header">{service}</div>
+                <hr/>
+                <div className="meta">{name}</div>
+                <div className="meta">{city}</div>
+                <div className="meta">{price}</div>
+                <div className="meta">{address}</div>
+                <div className="meta">{phone}</div>
+                <hr/>
+                <div className="meta">{website}</div>
             </div>
         </div>
     )
